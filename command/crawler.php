206e9d5f6dc0e118d72f00f34c3ba7678d99c0dd
yaoyang <kiki@smarty.so>
2017-07-31 22:38:27 +0800

    添加清除云币数据的命令

--- command/crawler.php
+++ command/crawler.php
@@ -12,3 +12,15 @@ command('crawler:yunbi', '从云币抓取数据', function () {
         storage_insert($dc_name, $data);
     }
 });
+
+command('crawler:yunbi-clean', '清除从云币抓取的数据', function () {
+
+    $infos = remote_get_json('https://yunbi.com/api/v2/tickers.json');
+
+    foreach ($infos as $dc_name => $info) {
+
+        storage_delete($dc_name);
+
+    }
+
+});
