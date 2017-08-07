01ad899a64611b66664c682053f4f5ddaaaa7779
yaoyang <kiki@smarty.so>
2017-08-07 13:11:36 +0800

    添加 markets 接口

--- controller/yunbi.php
+++ controller/yunbi.php
@@ -8,3 +8,10 @@ if_get('/yunbi/*/k/*', function ($dc_name, $period)
 
     return array_reverse(db_simple_query($table, [], 'order by at desc limit '.$limit));
 });
+
+if_get('/yunbi/markets', function () {
+
+    $infos = remote_get_json('https://yunbi.com//api/v2/markets.json');
+
+    return $infos;
+});
