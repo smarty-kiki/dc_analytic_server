b5acc4c0e875df13d9f26b74a58d54255a3898df
yaoyang <kiki@smarty.so>
2017-08-16 18:45:18 +0800

    添加 jubi 公告抓取异常捕获

--- command/queue_job/crawl_announcement.php
+++ command/queue_job/crawl_announcement.php
@@ -7,29 +7,34 @@ function crawl_announcement_table()
 
 queue_job('crawl_jubi_announcement', function ()
 {/*{{{*/
-    $jubi_domain = 'https://www.jubi.com';
+    try {
+        $jubi_domain = 'https://www.jubi.com';
 
-    $html = file_get_contents($jubi_domain.'/gonggao/');
-    $html = mb_convert_encoding($html, 'utf8', 'auto');
-    $dom = new html_parser($html);
+        $html = file_get_contents($jubi_domain.'/gonggao/');
+        $html = mb_convert_encoding($html, 'utf8', 'auto');
+        $dom = new html_parser($html);
 
-    $new_list = $dom->find('.new_list', 0);
-    $titles = $new_list->find('.title');
-    $titles = array_reverse($titles);
+        $new_list = $dom->find('.new_list', 0);
+        $titles = $new_list->find('.title');
+        $titles = array_reverse($titles);
 
-    foreach ($titles as $title) {
+        foreach ($titles as $title) {
 
-        $url = $jubi_domain.$title->href;
+            $url = $jubi_domain.$title->href;
 
-        if (! db_simple_query_first(crawl_announcement_table(), ['url' => $url])) {
-            db_simple_insert(crawl_announcement_table(), [
-                'title' => $title->plaintext,
-                'url' => $url,
-                'web' => 'jubi',
-                'at' => now(),
-            ]);
-            slack_say_to_smarty_dc($title->plaintext.' '.$url);
+            if (! db_simple_query_first(crawl_announcement_table(), ['url' => $url])) {
+                db_simple_insert(crawl_announcement_table(), [
+                    'title' => $title->plaintext,
+                    'url' => $url,
+                    'web' => 'jubi',
+                    'at' => now(),
+                ]);
+                slack_say_to_smarty_dc('jubi: '.$title->plaintext.' '.$url);
+            }
         }
+    } catch (Exception $ex) {
+        slack_say_to_smarty_dc('jubi: 数据抓取出问题了');
+        throw $ex;
     }
 
     return true;
