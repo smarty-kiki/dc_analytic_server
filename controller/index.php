db84113f0d7656b15009c80184a54d351c5d2ee9
yaoyang <kiki@smarty.so>
2017-08-20 18:02:25 +0800

    公告标题调整通知逻辑

--- controller/index.php
+++ controller/index.php
@@ -2,27 +2,5 @@
 
 if_get('/', function ()
 {
-    $domain = 'https://www.jubi.com';
-
-    $html = remote_get($domain.'/gonggao/', 10);
-
-    if (! $html) {
-        return false;
-    }
-
-    $html = mb_convert_encoding($html, 'utf8', 'auto');
-    $dom = str_get_html($html);
-
-    $new_list = $dom->find('.new_list', 0);
-    $anns = $new_list->find('.title');
-    $anns = array_reverse($anns);
-
-    foreach ($anns as $ann) {
-
-        $url = trim($domain.$ann->href);
-        $title= trim($ann->plaintext);
-
-        crawl_announcement_save_and_send_slack($title.'1', $url, 'jubi');
-    }
     return 'hello world';
 });
