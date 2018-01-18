c1a3a0c4d3869e08871aa6591e0bcd002dde4262
yaoyang <kiki@smarty.so>
2018-01-18 13:58:26 +0800

    1516254599999

--- command/queue_job/crawler_abnormal_volume.php
+++ command/queue_job/crawler_abnormal_volume.php
@@ -33,7 +33,7 @@ queue_job('crawler_bittrex_abnormal_volume', function ()
         otherwise($market_infos, '连接 coinmarketcap 不畅通');
 
         foreach ($market_infos as $rank => $info) {
-            $symbol = 'BTC-'.$info['symbol'];
+            $symbol = $info['symbol'];
 
             $ranks[$symbol] = $rank;
 
@@ -42,6 +42,10 @@ queue_job('crawler_bittrex_abnormal_volume', function ()
 
         foreach ($market_infos_indexed_by_symbol as $symbol => $info) {
             queue_push('crawler_bittrex_abnormal_volume_single', [
+                'symbol' => $symbol,
+                'rank' => $ranks[$symbol],
+            ]);
+            queue_push('crawler_binance_abnormal_volume_single', [
                 'symbol' => $symbol,
                 'rank' => $ranks[$symbol],
             ]);
@@ -58,7 +62,7 @@ queue_job('crawler_bittrex_abnormal_volume_single', function ($data)
     try {
         $step = 5;
 
-        $symbol = $data['symbol'];
+        $symbol = 'BTC-'.$data['symbol'];
         $rank = $data['rank'];
 
         // 拉币网数据
@@ -97,6 +101,72 @@ queue_job('crawler_bittrex_abnormal_volume_single', function ($data)
 
                         crawler_bittrex_abnormal_volume_slack_save_and_send_slack($symbol, $rank, $btc_volume, now($tick['T'].' +8 hours'), $btc_avg_volume, 
                             '*#'.$rank.' '.$symbol.' 币网 '.now($tick['T'].' +8 hours', 'H:i').'*'
+                            ."\n*5 分钟交易量 ".$btc_volume.'*'
+                            ."\n1 小时涨幅 ".$percent_change_1h.'%'
+                            ."\n24 小时涨幅 ".$percent_change_24h.'%'
+                            ."\n相比 24 小时内最高面值 ".$highest_price_percent_change_in_24h.'%'
+                            ."\n前 ".$step.' 柱平均交易量 '.$btc_avg_volume
+                            ."\n前 ".$step." 柱明细:\n  ".implode("\n  ", $bv_result)
+                        );
+                    }
+                }
+            }
+        }
+    } catch (Exception $ex) {
+        slack_say_to_smarty_ds($ex->getMessage());
+    }
+
+    return true;
+}, $priority = 10, $retry = [3, 3, 3], $tube = 'default', $config_key = 'default');/*}}}*/
+
+queue_job('crawler_binance_abnormal_volume_single', function ($data)
+{/*{{{*/
+    try {
+        $step = 5;
+
+        $symbol = $data['symbol'].'BTC';
+        $rank = $data['rank'];
+
+        // 拉币安数据
+        $res = remote_get_json('https://api.binance.com/api/v1/klines?symbol='.$symbol.'&interval=5m&limit=300', 10);
+
+        if (! array_key_exists('code', $res)) {
+
+            array_pop($res);
+
+            $result = $res;
+
+            $total_count = count($result);
+
+            foreach ($result as $index => $tick) {
+
+                if ($index > $total_count - 12) { // 一小时内的
+
+                    $btc_volume = (float) ($tick[5] * $tick[3]);
+
+                    $tmp_result = array_slice($result, $index - $step, $step);
+
+                    $bv_result = array_map($tmp_result, function ($v) {
+                        return (float) ($v[5] * $v[3]);
+                    });
+
+                    $btc_avg_volume = array_sum($bv_result) / count($bv_result);
+
+                    if ($btc_volume > $btc_avg_volume * 6 && $btc_volume > 10) {
+
+                        $high_price = (float) $tick[3];
+
+                        $percent_change_1h = round((($high_price - $result[$index - 12][3]) /$result[$index - 12][3] ) * 100, 1);
+                        $percent_change_24h = round((($high_price - $result[$index - 288][3]) /$result[$index - 288][3] ) * 100, 1);
+
+                        $tmp_result = array_slice($result, $index - 288, 288);
+                        $h_result = array_fetch($tmp_result, 3);
+
+                        $max_h_result = max($h_result);
+                        $highest_price_percent_change_in_24h = round((($high_price - $max_h_result) /$max_h_result) * 100, 1);
+
+                        crawler_bittrex_abnormal_volume_slack_save_and_send_slack($symbol, $rank, $btc_volume, now(($tick[6] / 1000)), $btc_avg_volume, 
+                            '*#'.$rank.' '.$symbol.' 币安 '.now(($tick[6] / 1000), 'H:i').'*'
                             ."\n*5 分钟交易量 ".$btc_volume.'*'
                             ."\n1 小时涨幅 ".$percent_change_1h.'%'
                             ."\n24 小时涨幅 ".$percent_change_24h.'%'
