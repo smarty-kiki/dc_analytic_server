ed11f5ad9b14bed1f9f416db9d88756a03b41079
yaoyang <kiki@smarty.so>
2018-04-08 21:45:36 +0800

    修改 after_push

--- project/tool/after_push.sh
+++ project/tool/after_push.sh
@@ -2,21 +2,11 @@
 
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../..
 
-cd $ROOT_DIR
+ln -fs $ROOT_DIR/project/config/production/nginx/dc_analytic_server.conf /etc/nginx/sites-enabled/dc_analytic_server
+/usr/sbin/service nginx reload
 
-OLD_VERSION=`/usr/bin/git rev-parse HEAD`
+/usr/bin/php $ROOT_DIR/public/cli.php migrate
 
-/usr/bin/git pull origin master
-
-NEW_VERSION=`/usr/bin/git rev-parse HEAD`
-
-if [ "$OLD_VERSION" != "$NEW_VERSION" ]; then
-    ln -fs $ROOT_DIR/project/config/production/nginx/dc_analytic_server.conf /etc/nginx/sites-enabled/dc_analytic_server
-    /usr/sbin/service nginx reload
-
-    /usr/bin/php $ROOT_DIR/public/cli.php migrate
-
-    ln -fs $ROOT_DIR/project/config/production/supervisor/dialogue_operator.conf /etc/supervisor/conf.d/dialogue_operator.conf
-    /usr/bin/supervisorctl update
-    /usr/bin/supervisorctl restart dialogue_operator:*
-fi
+ln -fs $ROOT_DIR/project/config/production/supervisor/dialogue_operator.conf /etc/supervisor/conf.d/dialogue_operator.conf
+/usr/bin/supervisorctl update
+/usr/bin/supervisorctl restart dialogue_operator:*
