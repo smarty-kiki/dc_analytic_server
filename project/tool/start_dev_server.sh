6d770116c6eb91412f7e60258bc2a1a213b61923
yaoyang <kiki@smarty.so>
2018-11-04 15:41:04 +0800

    更换 SCRIPT_DIR 为 ROOT_DIR

--- project/tool/start_dev_server.sh
+++ project/tool/start_dev_server.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"/../../
+ROOT_DIR="$(cd "$(dirname $0)" && pwd)"/../../
 
 sudo docker run --rm -ti -p 80:80 -p 3306:3306 --name dc_analytic_server \
-    -v $SCRIPT_DIR:/var/www/dc_analytic_server \
-    -v $SCRIPT_DIR/project/config/development/nginx/dc_analytic_server.conf:/etc/nginx/sites-enabled/default \
-    -v $SCRIPT_DIR/project/config/development/supervisor/dialogue_operator.conf:/etc/supervisor/conf.d/dialogue_operator.conf \
-    -v $SCRIPT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
+    -v $ROOT_DIR:/var/www/dc_analytic_server \
+    -v $ROOT_DIR/project/config/development/nginx/dc_analytic_server.conf:/etc/nginx/sites-enabled/default \
+    -v $ROOT_DIR/project/config/development/supervisor/dialogue_operator.conf:/etc/supervisor/conf.d/dialogue_operator.conf \
+    -v $ROOT_DIR/project/config/development/supervisor/queue_worker.conf:/etc/supervisor/conf.d/queue_worker.conf \
 kikiyao/debian_php_dev_env start
